7f2df98d5a80cba337e25e12d3311dbcd907e398
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-10-11 07:03:39 -0600

    final push

--- basic.ts
+++ basic.ts
@@ -1,4 +1,3 @@
-
 // primatives 
 let age: number;
 
@@ -8,6 +7,74 @@ let userName: string;
 
 userName = 'Sams4130'
 
-let isInstructor : boolean;
+let isInstructor: boolean;
 
 isInstructor = false;
+
+// complex types 
+let hobbies: string[];
+hobbies = ['Football', 'Basketball', 'Soccer'];
+
+// object type defeniton
+let person: {
+    name: string;
+    age: number;
+}; 
+person = {
+    name: "Maxy",
+    age: 32
+}
+//compination 
+// let people: {
+//     name: string;
+//     age: number;
+// }[];
+
+people = [
+    {name: 'John', age: 32},
+    {name: 'Alex', age: 12},
+    {name: 'Tammy',age: 40}
+]
+
+//type inference
+let course = 'React - a course for life'
+//course = 1,2,3,; this will generate an error
+course = 'Hello world';
+
+// union types 
+let studentClass: string | number = 'Woodworking';
+
+studentClass = 1234, 'Potery';
+
+// type aliases
+type Persons = {
+    name: string;
+    age: number;
+}; 
+
+let peps: Persons[];
+
+let people: Persons;
+
+// functions & types 
+function add(a:number,b:number) {
+    return a+b;
+}
+
+function printValue(value:string): number | any{
+    console.log(value);
+}
+
+//generics
+// util function
+function insertAtBegining<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const  demoArray = [1,2,3];
+
+const updatedArray = insertAtBegining(demoArray, -1); // -1,1,2,3 is output
+const stringArray = insertAtBegining(['a','b','c','d'], 'e')
+
+// updatedArray[0].split('');
